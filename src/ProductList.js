dde68b3bbe396d3ec6837cde4376a01800797439
Elambarathi k <156106606+Elambarathi@users.noreply.github.com>
2024-09-05 12:18:48 +0530

    Update ProductList.js

--- src/ProductList.js
+++ src/ProductList.js
@@ -6,7 +6,7 @@ function ProductList() {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/products/')
+        axios.get('https://django-deployment-vercel.vercel.app/api/products/')
             .then(response => {
                 setProducts(response.data);
             })
